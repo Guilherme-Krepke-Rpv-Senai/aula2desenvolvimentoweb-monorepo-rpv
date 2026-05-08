523b44f1f31c983dc7c7639680ad39f30b9f4cbc
Guilherme-krepke <0000765849@senaimgaluno.com.br>
2026-05-07 22:21:58 -0300

    feat: final da aula

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use(cors())
 
+const corsOptions = {
+    origin: "*"
+}
+
+app.use(cors(corsOptions))
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello, World!'})
 
